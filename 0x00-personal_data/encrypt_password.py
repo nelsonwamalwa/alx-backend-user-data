10b63bd9d199785c6dfd390a8e84f851ac4dfa54
NelsonWamalwa <wamalwanelson@gmail.com>
2024-05-16 15:15:39 +0300

    All tasks

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" doc doc doc """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The hashed password.
+    """
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """doc doc doc"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
