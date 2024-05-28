707b2cdba53bbb1b803f0823545bb8e363628097
NelsonWamalwa <wamalwanelson@gmail.com>
2024-05-28 22:41:30 +0300

    All the tasks

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-"""
-DB module
-"""
+"""DB module"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -13,34 +12,31 @@ from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
-        """Initialize new DB instance
-        """
-        self._engine = create_engine("sqlite:///a.db",
-                                     echo=False)
+        """Initialize new DB instance"""
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
+        """Memoized session object"""
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Creating a User object and save it to the database
+        """Creating a User object and save it to the database
+
         Args:
-            email (str): user's email address
-            hashed_password (str): password hashed by bcrypt's hashpw
-        Return:
+            email (str): User's email address
+            hashed_password (str): Password hashed by bcrypt's hashpw
+
+        Returns:
             Newly created User object
         """
         user = User(email=email, hashed_password=hashed_password)
@@ -49,13 +45,14 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """
-        Return a user who has an attribute matching the attributes passed
+        """Return a user who has an attribute matching the attributes passed
         as arguments
+
         Args:
-            attributes (dict): a dictionary of attributes to match the user
-        Return:
-            matching user or raise error
+            kwargs (dict): A dictionary of attributes to match the user
+
+        Returns:
+            Matching user or raise error
         """
         all_users = self._session.query(User)
         for k, v in kwargs.items():
@@ -67,14 +64,14 @@ class DB:
         raise NoResultFound
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """
-        Updating a user's attributes
+        """Updating a user's attributes
+
         Args:
-            user_id (int): user's id
-            kwargs (dict): dict of key, value pairs representing the
-                           attributes to update and the values to update
-                           them with
-        Return:
+            user_id (int): User's id
+            kwargs (dict): Dict of key, value pairs representing the
+                attributes to update and the values to update them with
+
+        Returns:
             No return value
         """
         try:
